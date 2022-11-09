58e5e5c2c70fa64ffce3ae0ef503f14f7feb6b57
Kingsley Victor <javaprodigy56@gmail.com>
2022-11-09 18:35:48 +0100

    ...

--- src/launchpad/router/index.ts
+++ src/launchpad/router/index.ts
@@ -2,18 +2,23 @@ import express, { Router } from "express";
 import _ from "lodash";
 import assert from "assert";
 import { privateTokenSales, publicTokenSales } from "../db/models";
+import rank from "../assets/rank.json";
 
 const getAllPublicSaleItems = async (req: express.Request, res: express.Response) => {
   try {
     const { query, params } = _.pick(req, ["query", "params"]);
     const allItemsCount = await publicTokenSales.countAllPublicSaleItems({ where: { chainId: params.chainId } });
+    const r = rank[parseInt(params.chainId) as unknown as keyof typeof rank];
     const allItems = await publicTokenSales.getAllPublicSaleItems({
       limit: 20,
       offset: query.page ? _.subtract(parseInt(query.page as string), 1) * 20 : 0,
       where: { chainId: params.chainId }
     });
     const result = {
-      items: allItems,
+      items: allItems.map(i => ({
+        ...i,
+        rank: r ? (r.gold.includes(i.id) ? "gold" : r.silver.includes(i.id) ? "silver" : r.bronze.includes(i.id) ? "bronze" : "unknown") : "unknown"
+      })),
       totalItems: allItemsCount
     };
     return res.status(200).json({ result });
@@ -26,13 +31,17 @@ const getAllPrivateSaleItems = async (req: express.Request, res: express.Respons
   try {
     const { query, params } = _.pick(req, ["query", "params"]);
     const allItemsCount = await privateTokenSales.countAllPrivateSaleItems({ where: { chainId: params.chainId } });
+    const r = rank[parseInt(params.chainId) as unknown as keyof typeof rank];
     const allItems = await privateTokenSales.getAllPrivateSaleItems({
       limit: 20,
       offset: query.page ? _.subtract(parseInt(query.page as string), 1) * 20 : 0,
       where: { chainId: params.chainId }
     });
     const result = {
-      items: allItems,
+      items: allItems.map(i => ({
+        ...i,
+        rank: r ? (r.gold.includes(i.id) ? "gold" : r.silver.includes(i.id) ? "silver" : r.bronze.includes(i.id) ? "bronze" : "unknown") : "unknown"
+      })),
       totalItems: allItemsCount
     };
     return res.status(200).json({ result });
